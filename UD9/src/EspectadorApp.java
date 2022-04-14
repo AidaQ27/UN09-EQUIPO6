fd44b315c2b4a77fbc2f209a89c1c32c9c264004
GerardSanchezV <gerardsanchezvelasco@gmail.com>
2022-04-14 11:00:57 +0200

    Espectador

--- UD9/src/EspectadorApp.java
+++ UD9/src/EspectadorApp.java
@@ -1,9 +1,49 @@
 
 public class EspectadorApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		//Atributos//
+		private  String nombre;
+		private int edad;
+		private double dinero;
+		
+		//Constructores//
+		public EspectadorApp(String nombre, int edad, double dinero) {
+			this.nombre = nombre;
+			this.edad = edad;
+			this.dinero = dinero;			
+		}	
+		//Metodos//
+		public String getNombre() {
+			return nombre;
+		}
+		public void setNombre(String nombre) {			
+			this.nombre = nombre;
+		}
+		public int getEdad() {
+			return edad;
+		}
+		public void setEdad (int edad) {
+			this.edad = edad;
+		}
+		public double getDinero() {
+			return dinero;
+		}
+		public void setDinero(double dinero) {
+			this.dinero = dinero;
+		}
+		public void pagar(double precio) {
+			dinero -= precio;
+		}
+		public boolean validarEdad(int edadMin) {
+			return edad >= edadMin;
+		}
+		public boolean validarDinero(double precioEntrada) {
+			return dinero >= precioEntrada;
+		}		
+		
+		public String toString() {
+			return "El nombre del espectador es " + nombre + ",tiene" + edad + " años y tiene " + dinero + "euros para pagar la entrada";
+		}
 }
+
+
